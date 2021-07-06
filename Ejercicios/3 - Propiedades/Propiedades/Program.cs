9a8ef925189283be6661115f54197b0350a3f346
Elvin Palma <enpalma@unah.hn>
2021-07-06 13:01:28 -0600

    Agregar Propiedades

---
+++ Ejercicios/3 - Propiedades/Propiedades/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Propiedades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Alumno a = new Alumno();
+            a.PrimerNombre = "Juan";
+            a.SegundoNombre = "Perez";
+
+            Alumno b = new Alumno();
+            b.Id = 2;
+            b.PrimerNombre = "Maria";
+            b.SegundoNombre = "Martinez";
+
+            Alumno c = new Alumno(3);
+            c.PrimerNombre = "Pedro";
+
+            Alumno d = new Alumno("Juan", "Jimenez");
+            
+            Console.WriteLine(a.Id);
+            Console.WriteLine(b.Id);
+            Console.WriteLine(c.Id);
+            Console.WriteLine(d.PrimerNombre + " " + d.SegundoNombre);
+            
+        }
+    }
+}
