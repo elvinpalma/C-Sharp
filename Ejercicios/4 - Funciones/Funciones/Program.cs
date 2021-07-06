8c102bde3b6f3345f1449714812ad08317f5c3da
Elvin Palma <enpalma@unah.hn>
2021-07-06 15:02:53 -0600

    Agregar Funciones

---
+++ Ejercicios/4 - Funciones/Funciones/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Funciones
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Alumno a = new Alumno(1, "Juan", "Perez");
+            Alumno b = new Alumno(2, "Maria", "Martinez");
+
+            a.inactivarAlumno();
+            
+            Console.WriteLine(a.NombreCompleto());
+            Console.WriteLine(b.NombreCompleto());
+
+            a.activarAlumno();
+
+            Console.WriteLine(" ");
+            
+            Console.WriteLine(a.NombreCompleto());
+            Console.WriteLine(b.NombreCompleto());
+        }
+    }
+}
