185bf135f099e959cb175efd013b8ab68b23a4b7
Elvin Palma <enpalma@unah.hn>
2021-07-11 04:07:06 -0600

    Agregar Ejercicio 2 Notas Escolares

--- Ejercicios/2 - Notas-Escolares/Program.cs
+++ Ejercicios/2 - Notas-Escolares/Program.cs
@@ -6,7 +6,18 @@ namespace Notas_Escolares
     {
         static void Main(string[] args)
         {
-            
+            Alumno alumno1 = new Alumno();
+            alumno1.Codigo = 1;
+            alumno1.Nombre = "Juan";
+            alumno1.ingresarNota();
+
+            Alumno a = new Alumno();
+            a.Codigo = 2;
+            a.Nombre = "Pedro";
+            a.ingresarNota();
+
+            Console.WriteLine("Alumno: " + alumno1.Nombre);
+            Console.WriteLine("Alumno: " + a.Nombre);
         }
     }
 }
