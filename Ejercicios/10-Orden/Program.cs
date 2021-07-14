a885fbfe28b4d6ddddb716442489168b740d7692
Elvin Palma <enpalma@unah.hn>
2021-07-14 01:47:43 -0600

    Agregar Ejercicio#9

---
+++ Ejercicios/10-Orden/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _10_Orden
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
